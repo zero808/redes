3b5fe34a7d1b5ef4fa1eaf75a9aa3206290881af
alunos <alunos@minho.tecnico.ulisboa.pt>
2015-09-15 16:35:11 +0100

    cc

--- server.c
+++ server.c
@@ -11,28 +11,26 @@
 #define PORT 59000
 
 int main() {
-	int fd, n, newfd, nw, addrlen;
-	struct sockaddr_in addr;
+	int fd, n, newfd, nw, addrlen, clientlen;
+	struct sockaddr_in addr, c_addr;
 	struct hostent *h;
 	char *ptr, buffer[128];
 
-	if((h=gethostbyname("lima")) == NULL) exit(1);
 	fd= socket(AF_INET, SOCK_STREAM, 0);
 	if(fd == -1) exit(1);
 	memset((void*) &addr, (int) '\0', sizeof(addr));
 	addr.sin_family = AF_INET;
-	//addr.sin_addr =
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	//addr.sin_addr.s_addr = ((struct in_addr *) (h->h_addr_list[0]))->s_addr;
-	addr.sin_port = htons(9000);
+	addr.sin_port = htons(PORT);
 
 	if(bind(fd, (struct sockaddr*) &addr, sizeof(addr)) ==-1)
 		exit(1);
 	if(listen(fd, 5) == -1) exit(1);
+//	clientlen = sizeof(c_addr);
 
 	while(1) {
-		addrlen = sizeof(addr);
-		if((newfd = accept(fd, (struct sockaddr*) &addr, &addrlen)) == -1)
+		clientlen = sizeof(c_addr);
+		if((newfd = accept(fd, (struct sockaddr*) &c_addr, &clientlen)) == -1)
 			exit(1);
 		while((n = read(newfd, buffer, 128)) != 0) {
 			if(n == -1) exit(1);
